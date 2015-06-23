561567f54224f76b42f68cabbd7499356195944c
KimJeongYeon <jeongyeon.kim@samsung.com>
2015-06-23 13:51:37 +0900

    gst-plugins-good/pulsesink sync code of tizen_2.4 branch
    [Version] 1.4.1
    [Profile] Common
    [Issue Type] Add features
    [Dependency module] iniparser
    [Dependency commit] NA
    [Comment]
    
    Signed-off-by: KimJeongYeon <jeongyeon.kim@samsung.com>
    Change-Id: Ia87520364a9b44ab73e87d1a694763cea0fa5ac1

--- ext/pulse/pulsesink.h
+++ ext/pulse/pulsesink.h
@@ -52,6 +52,16 @@ G_BEGIN_DECLS
 #define GST_PULSESINK_CAST(obj) \
   ((GstPulseSink *)(obj))
 
+#ifdef __TIZEN__
+enum {
+  PULSESINK_LOCAL_CONFIGURATION_LOW,
+  PULSESINK_LOCAL_CONFIGURATION_MID,
+  PULSESINK_LOCAL_CONFIGURATION_HIGH,
+  PULSESINK_LOCAL_CONFIGURATION_VERY_HIGH,
+  PULSESINK_LOCAL_CONFIGURATION_MAX = PULSESINK_LOCAL_CONFIGURATION_VERY_HIGH,
+};
+#endif
+
 typedef struct _GstPulseSink GstPulseSink;
 typedef struct _GstPulseSinkClass GstPulseSinkClass;
 
@@ -66,10 +76,19 @@ struct _GstPulseSink
 
   gchar *server, *device, *stream_name, *client_name;
   GstPulseDeviceInfo device_info;
+#ifdef __TIZEN__
+  gint volume_type;
+  gint latency;
+  gint user_route;
+#endif
 
   gdouble volume;
   gboolean volume_set:1;
+#ifdef __TIZEN__
+  gint mute;
+#else
   gboolean mute:1;
+#endif
   gboolean mute_set:1;
   guint32 current_sink_idx;
   gchar *current_sink_name;
@@ -77,6 +96,19 @@ struct _GstPulseSink
   guint defer_pending;
 
   gint notify; /* atomic */
+
+#ifdef __TIZEN__
+  gint fade_stat;
+  gint fade_duration;
+  gint support_type;
+  gboolean cork_on_prepare;
+  gint buffer_time[PULSESINK_LOCAL_CONFIGURATION_MAX];
+  gint latency_time[PULSESINK_LOCAL_CONFIGURATION_MAX];
+#ifdef PCM_DUMP_ENABLE
+  gint need_dump_input;
+  FILE *dump_fd_input;
+#endif
+#endif
 
   const gchar *pa_version;
 
