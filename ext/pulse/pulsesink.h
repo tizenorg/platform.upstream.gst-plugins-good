e00f737c073e551b29cb7f3817bcfb6c258b88d7
KimJeongYeon <jeongyeon.kim@samsung.com>
2015-07-30 15:15:42 +0900

    gst-plugins-good/pulsesink changed latency type to string
    [Version] 1.4.1
    [Profile] Common
    [Issue Type] Add features
    [Dependency module] NA
    [Dependency commit] NA
    [Comment]
    
    Signed-off-by: KimJeongYeon <jeongyeon.kim@samsung.com>
    Change-Id: Ie674d0efa1ac3708678867148c44d29a87ff3d58

--- ext/pulse/pulsesink.h
+++ ext/pulse/pulsesink.h
@@ -79,12 +79,12 @@ struct _GstPulseSink
   gint notify; /* atomic */
 
 #ifdef __TIZEN__
-  gint latency;
+  gchar *latency;
 #ifdef PCM_DUMP_ENABLE
   gint need_dump_input;
   FILE *dump_fd_input;
 #endif
-#endif
+#endif /* __TIZEN__ */
 
   const gchar *pa_version;
 
